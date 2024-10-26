ca9b6f4845ff063f8f5802ceafa6e1a937804606
FishShalami <fishshalami@gmail.com>
2024-10-25 22:44:08 -0700

    Complete game with logic and appropriate functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello world!")
+console.log("Hello! I want to play a game...")
 
 
 // get computer choice of options rock, paper or scissor
@@ -18,10 +18,9 @@ let getComputerChoice = () => {
     return compChoice
 }
 
-console.log('The computer chose: ' + getComputerChoice());
+// console.log('The computer chose: ' + getComputerChoice());
 
 // get human choice of rock, paper, or scissor through function
-    //use user input field. Make it drop down select so that they have to pick one
 
 let getHumanChoice = () => {
     huChoice = prompt("Choose either 'Rock', 'Paper', or 'Scissor'", "Rock");
@@ -29,46 +28,51 @@ let getHumanChoice = () => {
     return huChoice;
 }   
 
-console.log('The human chose: ' + getHumanChoice())
+// console.log('The human chose: ' + getHumanChoice())
 
 
-//score tracking will keep tally of who has won
+function playGame() {
+    //score tracking will keep tally of who has won
     //Create two new variables named humanScore and computerScore in the global scope.
 
-let humanScore = 0;
-let computerScore = 0;
+    let humanScore = 0;
+    let computerScore = 0;
 
-//logic for single round of play
-function playRound(humanChoice, computerChoice) {
-  
-    //draw?
-    if (humanChoice === computerChoice) {
-        console.log('Draw! Play again!');
-    //Rock (1) beats scissor (3) => 13/31 -> combine the words
-    //Paper (2) beats rock (1) => 21/12
-    //Scissor (3) beats paper (2) => 32/23
-    } else if (humanChoice + computerChoice === 'RockScissor' || 
-        humanChoice + computerChoice === 'PaperRock' ||
-        humanChoice + computerChoice === 'ScissorRock')  {
-            humanScore = ++humanScore;
-            console.log(`You won! ${humanChoice} beats ${computerChoice}`);
-    } else {
-        computerScore = ++computerScore;
-        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    
+    //logic for single round of play
+    let playRound = (humanChoice, computerChoice) => {
+      
+        //draw?
+        if (humanChoice === computerChoice) {
+            console.log('Draw! Play again!');
+        //Rock (1) beats scissor (3) => 13/31 -> combine the words
+        //Paper (2) beats rock (1) => 21/12
+        //Scissor (3) beats paper (2) => 32/23
+        } else if (humanChoice + computerChoice === 'RockScissor' || 
+            humanChoice + computerChoice === 'PaperRock' ||
+            humanChoice + computerChoice === 'ScissorRock')  {
+                humanScore = ++humanScore;
+                console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        } else {
+            computerScore = ++computerScore;
+            console.warn(`You lost! ${computerChoice} beats ${humanChoice}`);
+        }
+    
+        return humanScore;
+        return computerScore;
+    
     }
 
-    return humanScore;
-    return computerScore;
+    for (let i = 0; i < 5; i++) {
+        playRound(humanSelection, computerSelection);
+        console.log(`The score is Human: ${humanScore} Computer: ${computerScore}`);
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
 
+    }
     
-
 }
 
-
-//logic for entire game
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
+playGame();
